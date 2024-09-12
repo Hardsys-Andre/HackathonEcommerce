50b9a126b0efbea4fdb7a7875cd7260c3dc4aa1b
Andre Silva <hardsys_andre@hotmail.com>
2024-09-12 17:21:14 -0300

    fix-remoção do backend

--- src/frontend/src/sections/catalog/index.jsx
+++ src/frontend/src/sections/catalog/index.jsx
@@ -1,19 +1,11 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import ProductCarousel from "../../components/carrossel/index";
-import * as actions from "../../store/modules/products/actions";
 import { produtos } from "../../Data";
 
 const Catalog = () => {
-  const dispatch = useDispatch();
 
   const frutas = produtos.filter((p) => p.category === "Fruta");
   const verduras = produtos.filter((p) => p.category === "Verdura");
   const legumes = produtos.filter((p) => p.category === "Legume");
-
-  useEffect(() => {
-    dispatch(actions.getAllProductsRequest());
-  }, [dispatch]);
 
   return (
     <div className="flex flex-col w-full md:w-full pb-11 items-center bg-whiteNormal">
--- src/frontend/src/services/axios.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://tanacesta.azurewebsites.net/api/',
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
